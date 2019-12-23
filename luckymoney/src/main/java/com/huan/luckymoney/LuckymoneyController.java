883721f18aac118fc4d33b3ccf5dbd595d222b11
huanfuan <huanfuan@163.com>
2019-12-24 01:40:58 +0800

    jpa crud

---
+++ luckymoney/src/main/java/com/huan/luckymoney/LuckymoneyController.java
@@ -0,0 +1,73 @@
+package com.huan.luckymoney;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @Author huanfuan
+ * @Date 2019/12/24 1:12
+ * @Version 1.0
+ */
+
+@RestController
+public class LuckymoneyController {
+
+
+    @Autowired
+    private LuckymoneyRepository repository;
+
+    /*
+    * 获取红包列表
+    * */
+    @GetMapping("/luckymoneys")
+    public List<Luckymoney> list(){
+        return repository.findAll();
+    }
+
+
+
+    /*
+    *创建红包
+     */
+    @PostMapping("/luckymoneys")
+    public Luckymoney create(@RequestParam("producer") String producer,
+                             @RequestParam("money") BigDecimal money){
+        Luckymoney luckymoney=new Luckymoney();
+        luckymoney.setProducer(producer);
+        luckymoney.setMoney(money);
+        return repository.save(luckymoney);
+    }
+
+    /*
+     *通过id查询红包
+     */
+    @GetMapping("/luckymoneys/{id}")
+    public Luckymoney findById(@PathVariable("id") Integer id){
+        return repository.findById(id).orElse(null);
+    }
+
+    /*
+    *
+     更新红包
+     */
+    @PutMapping
+    public Luckymoney update(@PathVariable("id") Integer id,
+                            @RequestParam("consumer") String consumer){
+
+        Optional<Luckymoney> optional= repository.findById(id);
+
+        if (optional.isPresent()){
+            Luckymoney luckymoney= optional.get();
+            luckymoney.setConsumer(consumer);
+            return repository.save(luckymoney);
+        }
+
+        return null;
+
+    }
+
+}
---
+++ luckymoney/src/main/java/com/huan/luckymoney/LuckymoneyRepository.java
@@ -0,0 +1,12 @@
+package com.huan.luckymoney;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @Author huanfuan
+ * @Date 2019/12/24 1:13
+ * @Version 1.0
+ */
+public interface LuckymoneyRepository extends JpaRepository<Luckymoney,Integer> {
+
+}
