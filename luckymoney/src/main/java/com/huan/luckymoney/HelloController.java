34a73a43edad19aab8f256f78827e30836090cea
huanfuan <huanfuan@163.com>
2019-12-24 00:34:52 +0800

    RestController

--- luckymoney/src/main/java/com/huan/luckymoney/HelloController.java
+++ luckymoney/src/main/java/com/huan/luckymoney/HelloController.java
@@ -2,8 +2,8 @@ package com.huan.luckymoney;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
@@ -11,17 +11,24 @@ import java.math.BigDecimal;
  * @Author huanfuan
  * @Date 2019/12/23 23:24
  * @Version 1.0
+ * @Controller+@ResponseBody=@RestController
  */
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 
 
     @Autowired
     private LimitConfig limitConfig;
 
-    @GetMapping("/hello")
-    public String say(){
-        return" 说明： "+limitConfig.getDescription();
+    @GetMapping("/say")
+    public String say(@RequestParam(value = "id", required = false,defaultValue = "0") Integer id){
+
+//       return" 说明： "+limitConfig.getDescription();
+        return "id:"+id;
     };
+
+
+
 }
