c80793536fcd8a05a898fe5b841cde2d490a9366
huanfuan <huanfuan@163.com>
2019-12-24 00:50:26 +0800

    springboot-mybatis-redis

---
+++ springboot-mybatis-redis/src/test/java/com/example/springbootmybatisredis/SpringbootMybatisRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmybatisredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
